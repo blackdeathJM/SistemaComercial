79ed946d24aad3d014d9c7552f95e645d9d27cb7
blackdeath <blackdeath166@gmail.com>
2022-07-29 01:46:54 -0500

    Checando los graphQLErrors

--- src/shared/services/api.service.ts
+++ src/shared/services/api.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Apollo} from 'apollo-angular';
 import {DocumentNode} from 'graphql';
-import {catchError, finalize, Observable, of} from 'rxjs';
+import {catchError, Observable, of} from 'rxjs';
 import {NgxToastService} from '@shared/services/ngx-toast.service';
 import {GRAPHQL_STATE} from '@apollo/graphql.state';
 import {concat, isArray} from 'lodash-es';
@@ -40,21 +40,7 @@ export class ApiService
             refetchQueries
         }).pipe(catchError(() =>
         {
-            if (GRAPHQL_STATE())
-            {
-                if (isArray(GRAPHQL_STATE()))
-                {
-                    this.errorGraphql = GRAPHQL_STATE();
-                } else
-                {
-                    this.errorGraphql = concat(GRAPHQL_STATE());
-                }
-                this.errorGraphql.map((res) =>
-                {
-                    this.ngxToastService.errorToast(res, 'Error del servidor',
-                        {timeOut: 20000, progressAnimation: 'increasing', enableHtml: true, closeButton: true, progressBar: true});
-                });
-            }
+            this.errorServidor();
             return of([]);
         }));
     }
@@ -66,5 +52,24 @@ export class ApiService
             variables,
             context
         }).pipe(catchError(err => of([err])));
+    }
+
+    private errorServidor(): void
+    {
+        if (GRAPHQL_STATE())
+        {
+            if (isArray(GRAPHQL_STATE()))
+            {
+                this.errorGraphql = GRAPHQL_STATE();
+            } else
+            {
+                this.errorGraphql = concat(GRAPHQL_STATE());
+            }
+            this.errorGraphql.map((res) =>
+            {
+                this.ngxToastService.errorToast(res, 'Error del servidor',
+                    {timeOut: 20000, progressAnimation: 'increasing', enableHtml: true, closeButton: true, progressBar: true});
+            });
+        }
     }
 }
