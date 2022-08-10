b945a23f33d93742d13c73f2d503f142daa0a00f
blackdeath <blackdeath166@gmail.com>
2022-08-10 06:06:08 -0500

    Departamentos registro

--- apps/sistema-comercial/src/app/modules/admin/deptos/components/mod-depto/mod-depto.component.ts
+++ apps/sistema-comercial/src/app/modules/admin/deptos/components/mod-depto/mod-depto.component.ts
@@ -3,7 +3,7 @@ import {RxFormBuilder} from '@rxweb/reactive-form-validators';
 import {FormGroup} from '@angular/forms';
 import {MatDialog} from '@angular/material/dialog';
 import {NgxToastService} from '#/libs/services/src/lib/ngx-toast.service';
-import {Depto} from '@s-app/deptos/model/depto';
+import {Depto} from '@s-app/deptos/depto';
 import {CrearDeptoGQL} from '#/libs/datos/src/lib/admin/depto/codeGenDepto';
 import {STATE_DEPTOS} from '@s-app/deptos/deptos.state';
 import {IDepto} from '#/libs/models/src';
@@ -32,25 +32,7 @@ export class ModDeptoComponent implements OnInit
 
     registrar(): void
     {
-        // this.deptosService.crearDepto(this.formDepto.value).pipe(finalize(() =>
-        // {
-        //     GRAPHQL_STATE(null);
-        //     this.cargandoDatos = false;
-        //     this.cancelar();
-        // }), tap((res) =>
-        // {
-        //     if (!GRAPHQL_STATE())
-        //     {
-        //         console.log(res);
-        //         if (res.data)
-        //         {
-        //             const elementos = STATE_DEPTOS();
-        //             STATE_DEPTOS([...elementos, res.data['crearDepto']]);
-        //             this.ngxToast.satisfactorioToast('El registro se ha creado correctamente', 'Registro de departamentos');
-        //         }
-        //     }
-        // })).subscribe();
-
+        this.cargandoDatos = true;
         this.crearDeptoGQL.mutate({input: this.formDepto.value}, {
             useMutationLoading: true,
             // update: (store, result) =>
@@ -59,7 +41,7 @@ export class ModDeptoComponent implements OnInit
             //     data.deptos = [...data.deptos, result.data.crearDepto];
             //     store.writeQuery({query: DepartamentosDocument, data});
             // }
-        }).pipe(finalize(() => this.ngxToast.satisfactorioToast('El registro se realizo satisfoaci', 'Registro'))).subscribe((res) =>
+        }).pipe(finalize(() => this.cancelar())).subscribe((res) =>
         {
             if (res.data)
             {
