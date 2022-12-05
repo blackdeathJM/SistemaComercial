9982c35477ddc8cf9825ded609c152bec5924be1
Jose Omar <blackdeath166@gmail.com>
2022-12-05 07:32:36 -0600

    Modificando el layout para aumentar el tamaño del avatar

--- apps/sistema-comercial/src/app/modules/admin/empleado/components/perfil/perfil.component.ts
+++ apps/sistema-comercial/src/app/modules/admin/empleado/components/perfil/perfil.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {MatIconModule} from "@angular/material/icon";
+import {Component} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MaterialFileInputModule} from 'ngx-material-file-input';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
     selector: 'app-perfil',
     standalone: true,
-    imports: [CommonModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
+    imports:
+        [
+            CommonModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MaterialFileInputModule, MatDividerModule, NgOptimizedImage
+        ],
     templateUrl: './perfil.component.html',
     styleUrls: ['./perfil.component.scss'],
 })
-export class PerfilComponent {}
+export class PerfilComponent
+{
+    srcImagen: string = null;
+
+    cambioImagen(e: Event): void
+    {
+        const leer = new FileReader();
+        const [file] = e.target['files'];
+        console.log('*******', typeof file);
+        leer.readAsDataURL(file);
+
+        leer.onload = (): string => this.srcImagen = leer.result as string;
+        console.log('===>', e.target['files']);
+    }
+}
