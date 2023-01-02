fd0645a140b7c132b5758dcf186b83793d5fce17
Jose Omar <blackdeath166@gmail.com>
2023-01-02 03:00:57 -0600

    Corregiendo login, porque la falta de redireccion

--- apps/sistema-comercial/src/ngxs/ngxsGlobal.module.ts
+++ apps/sistema-comercial/src/ngxs/ngxsGlobal.module.ts
@@ -1,19 +1,17 @@
 import {NgModule} from '@angular/core';
 import {NgxsModule} from '@ngxs/store';
 import {environment} from '@s-environments/environment';
-import {DeptoEliminarStore} from '#/apps/sistema-comercial/src/query/deptoEliminar.store';
 import {NgxsDataPluginModule} from '@angular-ru/ngxs';
 import {NGXS_DATA_STORAGE_CONTAINER, NGXS_DATA_STORAGE_PLUGIN} from '@angular-ru/ngxs/storage';
 import {StateAuth} from '@s-core/auth/auth.store';
 import {EmpleadosStore} from '@s-dirAdmonFinanzas/empleados/empleados.store';
-import {DeptoEntitieState} from '@s-admin/e-depto.store';
+import {EDeptoStore} from '@s-admin/e-depto.store';
 
 @NgModule({
     imports:
         [
             NgxsModule.forRoot(
-                [
-                    DeptoEliminarStore, DeptoEntitieState, StateAuth, EmpleadosStore
+                [EDeptoStore, StateAuth, EmpleadosStore
                 ], {
                     developmentMode: !environment.production
                 }),
