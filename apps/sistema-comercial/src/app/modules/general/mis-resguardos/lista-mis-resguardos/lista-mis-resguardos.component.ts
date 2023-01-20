20f27220fe6c6573dadf5eb753e84323d8a3473f
Jose Omar <blackdeath166@gmail.com>
2023-01-20 03:52:54 -0600

    Resguardos borrador

--- apps/sistema-comercial/src/app/modules/general/mis-resguardos/lista-mis-resguardos/lista-mis-resguardos.component.ts
+++ apps/sistema-comercial/src/app/modules/general/mis-resguardos/lista-mis-resguardos/lista-mis-resguardos.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
     selector: 'app-lista-mis-resguardos',
     standalone: true,
-    imports: [CommonModule, MatCardModule],
+    imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule],
     templateUrl: './lista-mis-resguardos.component.html',
     styleUrls: ['./lista-mis-resguardos.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
