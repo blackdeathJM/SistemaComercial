c5774d2470e051305d0bdb9e7e7c3ebef961ba40
Jose Omar <blackdeath166@gmail.com>
2023-03-09 05:56:34 -0600

    Planeacion

--- apps/sistema-comercial/src/app/modules/dir-general/planeacion/mir/lista-tab-mir/lista-tab-mir.component.ts
+++ apps/sistema-comercial/src/app/modules/dir-general/planeacion/mir/lista-tab-mir/lista-tab-mir.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatCardModule} from '@angular/material/card';
