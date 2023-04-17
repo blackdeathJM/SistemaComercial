4480c9422df491725c2c2b2d26516702e0acf264
Jose Omar <blackdeath166@gmail.com>
2023-04-17 12:57:42 -0600

    Planeacion

--- apps/sistema-comercial/src/app/modules/dir-general/planeacion/mir/store/mir.query.ts
+++ apps/sistema-comercial/src/app/modules/dir-general/planeacion/mir/store/mir.query.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {QueryEntity} from '@datorama/akita';
 import {IMirState, MirStore} from '@s-dir-general/mir/store/mir.store';
-import {IMir} from '#/libs/models/src/lib/dir-general/planeacion/mir/mir.interface';
 
 @Injectable({providedIn: 'root'})
 export class MirQuery extends QueryEntity<IMirState, IMir>
