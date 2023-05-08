16920bbee91bb2acdd834a624a14a582a6962d50
Jose Omar <blackdeath166@gmail.com>
2023-05-07 19:16:34 -0600

    Empezando el diseño para el componente que esta conformado de cada uno de los indicadores del MIR

--- libs/models/src/lib/dir-general/planeacion/componentes/componente.interface.ts
+++ libs/models/src/lib/dir-general/planeacion/componentes/componente.interface.ts
@@ -1,5 +1,14 @@
 export interface IComponente
 {
     idComponente: string;
+}
 
+export interface ICompGeneral
+{
+    idCentroGestor: string;
+    descripcion: string;
+    valor: number;
+    formulaValor: string;
+    avancePrimerTrim: number;
+    formulaAvanceTrim: string;
 }
