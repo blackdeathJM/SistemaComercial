cdad5d7f3fc0f27b5298c2d02b83c465ad8d70d4
Jose Omar <blackdeath166@gmail.com>
2023-05-15 23:57:17 -0600

    Se va a empezar a crear los componentes

--- libs/models/src/lib/dir-general/planeacion/componentes/componente.interface.ts
+++ libs/models/src/lib/dir-general/planeacion/componentes/componente.interface.ts
@@ -1,28 +1,4 @@
-export interface IValoresAdicionales
-{
-    campo: string;
-    valor: number;
-}
-
-export interface formUno
-{
-    id: string;
-    idIndicador: string;
-}
-
-export interface IComponente
-{
-    trim1: number;
-    trim2: number;
-    trim3: number;
-    trim4: number;
-    formulaTrim: string;
-    tipoForm: number;
-    formUno: formUno[];
-    valoresAdicionales: IValoresAdicionales[];
-}
-
-export interface IComponentePlantas
+export interface IFormPlanta
 {
     idIndicador: string;
     ptarE: string;
@@ -94,4 +70,58 @@ export interface IComponentePlantas
     trim2: number;
     trim3: number;
     trim4: number;
+}
+
+//La forma que se va a calcular el Avance trimestral es dividiento el idIndicador entre el otro idIndicador
+export interface IForm1
+{
+    // Es el idIndicador del cuestonario PBR
+    idIndicador: string;
+    descripcion: string;
+
+    valorTrim1: IDefCampoValor;
+    valorTrim2: IDefCampoValor;
+    valorTrim3: IDefCampoValor;
+    valorTrim4: IDefCampoValor;
+
+    etiquetaValor: string;
+
+    avanceTrim1: number;
+    avanceTrim2: number;
+    avanceTrim3: number;
+    avanceTrim4: number;
+    etiquetaTrim: string;
+}
+
+export interface IForm2 extends IForm1
+{
+    campoAntTrim1: IDefCampoValor;
+    campoAntTrim2: IDefCampoValor;
+    campoAntTrim3: IDefCampoValor;
+    campoAntTrim4: IDefCampoValor;
+
+}
+
+export interface IDefCampoValor
+{
+    defCampo: string;
+    valorCampo: number;
+}
+
+export interface ICamposAdicionales
+{
+    titulo: string;
+    campo: IDefCampoValor[];
+}
+
+// export interface IComponente
+// {
+//     //Establecemos que tipo de formulario se va a usar en el componente
+//     tipoForm: number;
+// }
+
+export interface IComponente
+{
+    cabecera: string[];
+    valor: string[];
 }
