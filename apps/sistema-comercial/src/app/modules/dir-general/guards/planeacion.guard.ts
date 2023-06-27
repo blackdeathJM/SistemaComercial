51ec2ea01284d1fbffa8727a1ea0e07f4eb6c58d
Jose Omar <blackdeath166@gmail.com>
2023-06-27 01:03:35 -0600

    Se agrego el id del indicador, al objeto que tendra los datos del formulario, para asi sea mas facil obtener el valor del Id cuando se forme el obj para mostrar los datos en la tabla

--- apps/sistema-comercial/src/app/modules/dir-general/guards/planeacion.guard.ts
+++ apps/sistema-comercial/src/app/modules/dir-general/guards/planeacion.guard.ts
@@ -3,11 +3,11 @@ import {inject} from "@angular/core";
 import {AuthQuery} from "@s-core/auth/store/auth.query";
 import {ToastrService} from "ngx-toastr";
 
-export const planeacionGuard: CanActivateFn = (route, state) =>
+export const planeacionGuard: CanActivateFn = () =>
 {
     let usuario = inject(AuthQuery);
     const toast = inject(ToastrService);
-    if (usuario.getValue()._id === '63862985c8a23337dbe6ed31')
+    if (usuario.getValue()._id === '63862985c8a23337dbe6ed31' || usuario.getValue().auth.usuario === 'administrador')
     {
         return true;
     } else
@@ -15,5 +15,4 @@ export const planeacionGuard: CanActivateFn = (route, state) =>
         toast.warning('No tienes permiso para acceder a esta ruta', 'Permiso denegado');
         return false
     }
-
 };
