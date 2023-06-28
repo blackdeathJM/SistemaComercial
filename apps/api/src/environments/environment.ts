6a05ede7b4d82f9985a4814c74f86ff84745acb8
Jose Omar <blackdeath166@gmail.com>
2023-06-28 06:01:25 -0600

    primer revision de planeacion

--- apps/api/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    port: process.env.PORT,
-    palabraSecreta: process.env.PALABRA_SECRETA,
-    uriMongo: 'mongodb+srv://blackdeath:FernandaTeamo1017@simapas-api-k3zc5.mongodb.net/simapas-api?retryWrites=true&w=majority',
-};
