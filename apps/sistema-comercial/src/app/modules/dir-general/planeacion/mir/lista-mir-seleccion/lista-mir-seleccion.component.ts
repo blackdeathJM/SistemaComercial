d0b3bd9ebaf8d6c682bddb781894087885d36c26
Jose Omar <blackdeath166@gmail.com>
2023-07-22 06:26:39 -0600

    quitando algunos servicios obsoletos

--- apps/sistema-comercial/src/app/modules/dir-general/planeacion/mir/lista-mir-seleccion/lista-mir-seleccion.component.ts
+++ apps/sistema-comercial/src/app/modules/dir-general/planeacion/mir/lista-mir-seleccion/lista-mir-seleccion.component.ts
@@ -1,10 +1,9 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatExpansionModule} from "@angular/material/expansion";
 import {MatButtonToggleChange, MatButtonToggleModule} from "@angular/material/button-toggle";
 import {IMirCuestionario} from "#/libs/models/src/lib/dir-general/planeacion/mir/mir.interface";
 import {PlaneacionQuery} from "@s-dir-general/store/planeacion.query";
-import {AuthQuery} from "@s-core/auth/store/auth.query";
 
 @Component({
     selector: 'app-lista-mir-seleccion',
